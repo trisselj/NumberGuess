ff296092aacec6470cb3d2c99d00720e30413d91
trisselj <trisselj@oregonstate.edu>
2024-07-10 15:06:44 -0700

    Main

---
+++ num_guess.py
@@ -0,0 +1,28 @@
+# Author: Jake Trissel
+# Github Username: trisselj
+# Date: 07/10/2024
+# Description: Reads inputs and compares to initial number printing whther input is > < or = initial number
+
+# Initial prompt for number to guess for
+Target = int(input("Enter the integer for the player to guess."))
+
+#Keeps track of guesses
+guess_count = 0
+guess = None
+
+# Gameplay loop for guessing
+while True:
+    guess = int(input("Enter your guess"))
+    guess_count += 1
+    if guess == Target:
+        break
+    elif guess > Target:
+        print("Your guess is too high, please try again")
+    else:
+        print("Your guess is too low,please try again")
+
+#Prints out number of guesses
+if guess_count == 1:
+    print(f"You guessed it in 1 try.")
+else:
+    print(f"You guessed it in {guess_count} tries!")
